a19f43f844f51f2f6910236b96057c6c7aac3168
ffloresibarra <fflores@unitedvirtualities.com>
2019-12-19 11:06:56 -0700

    Comprender los conceptos básicos de jQuery

---
+++ jQuery-UI/5-effects/scripts/page.js
@@ -0,0 +1,32 @@
+﻿/// <reference path="jquery-1.6.2.js" />
+/// <reference path="jquery-ui-1.8.16.custom.js" />
+
+$(function () {
+
+    
+    // $("#target").click(function () {
+        // $(this).effect("explode", {pieces:100}, 2000);
+            // $(this).animate({
+            //     "width": "+=20",
+            //     "height": "+=20",
+            //     "background-color": "red"
+            // })
+        // });
+
+    $("#target")
+        .css("position", "relative")
+        .click(function () {
+            $(this).toggleClass("leftTarget", "slow");
+        });
+
+    $("#draggables").children().draggable();
+
+    $("#trash").droppable({
+        activeClass: "opaque",
+        drop: function (event, ui) {
+            ui.draggable.hide("explode");
+        }
+    });
+});
+
+
